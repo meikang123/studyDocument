b04a21e214072c1b6d05cc59f665e4dd23c9352c
meikang <meikang@ztgame.com>
2020-05-28 18:22:15 +0800

    --

---
+++ my-vue/src/layout/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="app-wrapper">
+        <div class="main-container">
+            <div class="app-main">
+                <router-view></router-view>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {}
+        }
+    };
+</script>
---
+++ my-vue/src/views/dashboard/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>Dashboard</div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {}
+        }
+    };
+</script>
